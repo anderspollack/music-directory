3310fc424f5a73fc647814134283bdd2a9eabe68
Jack Vaught <johnmvaught@gmail.com>
2020-01-14 14:52:10 -0800

    server updates

--- server/dist/routes/auth.js
+++ server/dist/routes/auth.js
@@ -5,8 +5,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
-router.get("/userX", (req, res) => {
-    res.send("user invalid");
+const body_parser_1 = __importDefault(require("body-parser"));
+const jsonParser = body_parser_1.default.json();
+router.post("/login", (req, res) => {
+    res.send("login");
+});
+router.delete("/logout", (req, res) => {
+    res.send("logout");
+});
+router.post("/register", jsonParser, (req, res) => {
+    const { username, password } = req.body;
+    res.send(`username: ${username} password: ${password}`);
+});
+router.delete("/delete", (req, res) => {
+    res.send("delete");
+});
+router.put("/update", (req, res) => {
+    res.send("update");
 });
 exports.default = router;
 //# sourceMappingURL=auth.js.map
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,8 +1,27 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/userX", (req, res) => {
-    res.send("user invalid");
+import bodyParser from "body-parser";
+const jsonParser = bodyParser.json();
+
+router.post("/login", (req, res) => {
+    res.send("login");
+});
+
+router.delete("/logout", (req, res) => {
+    res.send("logout");
+});
+
+router.post("/register", jsonParser, (req, res) => {
+    const { username, password } = req.body;
+});
+
+router.delete("/delete", (req, res) => {
+    res.send("delete");
+});
+
+router.put("/update", (req, res) => {
+    res.send("update");
 });
 
 export default router;
